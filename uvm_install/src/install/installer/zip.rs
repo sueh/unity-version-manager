fdc4b49261f3d0d18afc4d927938c58cfacc7c00
Sorrento Huang <srsiren@gmail.com>
2026-08-13 09:46:59 +0800

    Format module merge regression test

--- uvm_install/src/install/installer/zip.rs
+++ uvm_install/src/install/installer/zip.rs
@@ -144,11 +144,8 @@ mod tests {
         archive.write_all(b"new").unwrap();
         archive.finish().unwrap();
 
-        let installer = ModuleZipInstaller::new(
-            &archive_path,
-            &destination,
-            None::<(PathBuf, PathBuf)>,
-        );
+        let installer =
+            ModuleZipInstaller::new(&archive_path, &destination, None::<(PathBuf, PathBuf)>);
         installer.install().unwrap();
 
         assert_eq!(
